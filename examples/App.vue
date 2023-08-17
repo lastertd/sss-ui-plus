5b68e60909711761a4d0e1f4b65a39cd7635e60e
laster <lasterxin@163.com>
2023-08-17 14:22:57 +0800

    update

--- examples/App.vue
+++ examples/App.vue
@@ -1,34 +1,51 @@
 <template>
 
+
 	<s-button @click="handleClick">click me</s-button>
-	<s-skeleton :loading="f" :throttle="1000">
-		<template #skeleton>
-			<div class="container">
-				<s-skeleton-item  ></s-skeleton-item>
-				<s-skeleton-item style="width: 30%" ></s-skeleton-item>
-				<s-skeleton-item style="width: 100px;height: 100px" type="image" is-circle ></s-skeleton-item>
-			</div>
+	<div class="container_">
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div>
+			<div class="item" ref="item"></div>
 
-		</template>
-		hello
+		</div>
+	</div>
 
-	</s-skeleton>
+	<div style="height: 500px"></div>
 
 
 </template>
 
 <script setup lang="ts">
 
+import {onMounted, ref} from "vue";
+import {unrefElement, useElementVisibility} from "@vueuse/core";
 
-import {provide, ref} from "vue";
+const item = ref();
+const targetIsVisible = useElementVisibility(item)
 
-
-const f = ref(true);
-
-const handleClick = () => {
-	f.value = !f.value
+const getScrollParent = function (el:HTMLElement):HTMLElement {
+	const fa = el.parentNode as HTMLElement;
+	const overflow = window.getComputedStyle(fa).overflow;
+	if (fa === document.body || el === document.body) return document.body;
+	if (overflow === 'auto' || overflow === 'scroll') return fa;
+	return getScrollParent(fa);
 }
 
+
+const handleClick = () => {
+	const el = unrefElement(item) as HTMLElement;
+	const parent = getScrollParent(el) as HTMLElement;
+	console.log(el.offsetTop, el.getBoundingClientRect().top, parent.offsetHeight+parent.getBoundingClientRect().top)
+	console.log(targetIsVisible.value)
+
+
+	console.log(getScrollParent(el))
+}
 
 
 
@@ -37,6 +54,7 @@ const handleClick = () => {
 
 <style lang="less">
 @import "src/styles/global";
+
 #app {
 	display: flex;
 	flex-flow: column nowrap;
@@ -46,13 +64,22 @@ const handleClick = () => {
 	color: var(--sss-color-black-light);
 }
 
-.container{
+.container_ {
 	.useBorderBox();
 	.useBorderRadius5px();
 	.useBorderGray();
-	min-height: 500px;
-	min-width: 700px;
+	height: 200px;
+	width: 700px;
 	padding: 20px;
+	overflow: auto;
+}
+
+.item {
+	.useBorderBox();
+	.useBorderRadius5px();
+	.useBorderGrayDarker();
+	margin: 10px 0;
+	height: 100px;
 }
 
 
